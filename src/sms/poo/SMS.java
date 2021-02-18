cc2cedf556e2400ac0981a37c9ed00e5a4248f8c
User <User@eEuler>
2021-02-18 11:28:19 -0500

    incluyendo un saludo

--- src/sms/poo/SMS.java
+++ src/sms/poo/SMS.java
@@ -18,7 +18,7 @@ public class SMS {
         // TODO code application logic here
         System.out.println("Buenos dias");
         System.out.println("Mayra025");
-
+        System.out.println("jajajaj");
     }
     
 }
